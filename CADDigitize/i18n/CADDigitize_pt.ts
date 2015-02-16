d2bff7b399b273457519fd47c63a2d7b96b563ac
narceliodesa <narceliosapereira@gmail.com>
2015-02-16 18:10:24 -0300

    Translation for portuguese

---
+++ CADDigitize/i18n/CADDigitize_pt.ts
@@ -0,0 +1,242 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="pt_BR">
+<context>
+    <name>CADDigitize_ND</name>
+    <message>
+        <source>Add</source>
+        <translation>Adicionar</translation>
+    </message>
+    <message>
+        <source>CADDigitize - Numercial Digitize</source>
+        <translation>CADDigitize - Numercial Digitize</translation>
+    </message>
+    <message>
+        <source>Option</source>
+        <translation>Opção</translation>
+    </message>
+    <message>
+        <source>Remove</source>
+        <translation>Remover</translation>
+    </message>
+    <message>
+        <source>Point 1</source>
+        <translation>Ponto 1</translation>
+    </message>
+    <message>
+        <source>Point 2</source>
+        <translation>Ponto 2</translation>
+    </message>
+    <message>
+        <source>Point 3</source>
+        <translation>Ponto 3</translation>
+    </message>
+    <message>
+        <source>Function</source>
+        <translation>Função</translation>
+    </message>
+</context>
+<context>
+    <name>CADDigitizeSettings</name>
+    <message>
+        <source>Arcs</source>
+        <translation>Arcos</translation>
+    </message>
+    <message>
+        <source>CounterClockWise</source>
+        <translation>sentido anti-horário</translation>
+    </message>
+    <message>
+        <source>Angle</source>
+        <translation>Ângulo</translation>
+    </message>
+    <message>
+        <source>Chord</source>
+        <translation>Corda</translation>
+    </message>
+    <message>
+        <source>Pitch</source>
+        <translation>Afastamento</translation>
+    </message>
+    <message>
+        <source>Quadrant segment</source>
+        <translation>Quadrante de Segmento</translation>
+    </message>
+    <message>
+        <source>Circle</source>
+        <translation>Circulo</translation>
+    </message>
+    <message>
+        <source>ClockWise</source>
+        <translation>Sentido horário</translation>
+    </message>
+    <message>
+        <source>Angle Direction</source>
+        <translation>Direção Angular</translation>
+    </message>
+    <message>
+        <source>Polygon creation</source>
+        <translation>Criaçao de poligono</translation>
+    </message>
+    <message>
+        <source>Degree(s) </source>
+        <translation>Grau(s)</translation>
+    </message>
+    <message>
+        <source>Regular polygon</source>
+        <translation>Poligono regular</translation>
+    </message>
+    <message>
+        <source>Pie segment</source>
+        <translation>Segmento da pizza</translation>
+    </message>
+    <message>
+        <source>Millimeter(s) </source>
+        <translation>Milimetro(s)</translation>
+    </message>
+    <message>
+        <source>Feature Segmentation</source>
+        <translation>Segmentaçao da Feição</translation>
+    </message>
+    <message>
+        <source>Number of edges</source>
+        <translation>Número de bordas</translation>
+    </message>
+    <message>
+        <source>CADDigitize Settings</source>
+        <translation>CADDigitize Configuração</translation>
+    </message>
+    <message>
+        <source>Ellipse</source>
+        <translation>Elipse</translation>
+    </message>
+    <message>
+        <source>Number of points on ellipse</source>
+        <translation>Número de pontos da elipse</translation>
+    </message>
+</context>
+<context>
+    <name>CADDigitize</name>
+    <message>
+        <source>Help</source>
+        <translation>Ajuda</translation>
+    </message>
+    <message>
+        <source>Error</source>
+        <translation>Error</translation>
+    </message>
+    <message>
+        <source>Rectangle by extent</source>
+        <translation>Retângulo por medida</translation>
+    </message>
+    <message>
+        <source>Arc by center and 2 points</source>
+        <translation>Arco pelo centro e 2 pontos</translation>
+    </message>
+    <message>
+        <source>Ellipse by extent</source>
+        <translation>Ellipse por medida</translation>
+    </message>
+    <message>
+        <source>Arc by 3 points</source>
+        <translation>Arco por 3 pontos</translation>
+    </message>
+    <message>
+        <source>Ellipse by foci and a point</source>
+        <translation>Ellipse por foco e um ponto</translation>
+    </message>
+    <message>
+        <source>Segments are parallels</source>
+        <translation>Os segmentos são paralelos</translation>
+    </message>
+    <message>
+        <source>Regular polygon by center and point</source>
+        <translation>Polígono regular pelo centro e ponto</translation>
+    </message>
+    <message>
+        <source>Square from center</source>
+        <translation>Quadrado por centro</translation>
+    </message>
+    <message>
+        <source>Circle by 3 points</source>
+        <translation>Circulo por 3 pontos</translation>
+    </message>
+    <message>
+        <source>Circle by 2 points</source>
+        <translation>Circulo por 2 pontos</translation>
+    </message>
+    <message>
+        <source>Ellipse by center and 2 points</source>
+        <translation>Elipse pelo centro e 2 pontos</translation>
+    </message>
+    <message>
+        <source>Ellipse by center and 3 points</source>
+        <translation>Elipse pelo centro e 3 pontos</translation>
+    </message>
+    <message>
+        <source>Numerical Digitize</source>
+        <translation>Digitalizaçao numérica</translation>
+    </message>
+    <message>
+        <source>All fields are not valid</source>
+        <translation>
+Todos os campos não são válidos</translation>
+    </message>
+    <message>
+        <source>Regular polygon by 2 corners</source>
+        <translation>Polígono regular por 2 cantos</translation>
+    </message>
+    <message>
+        <source>Circle by center and radius</source>
+        <translation>Circulo por centro e raio</translation>
+    </message>
+    <message>
+        <source>Number of quadrant segments</source>
+        <translation>Numero de quadrantes do segmento</translation>
+    </message>
+    <message>
+        <source>Rectangle by 3 points</source>
+        <translation>Retângulo por 3 pontos</translation>
+    </message>
+    <message>
+        <source>Rectangle from center</source>
+        <translation>Retangulo por centro</translation>
+    </message>
+    <message>
+        <source>Arc by center, point and angle</source>
+        <translation>Arco por centro, ponto e angulo</translation>
+    </message>
+    <message>
+        <source>Circle by center and a point</source>
+        <translation>Circulo por centro e ponto</translation>
+    </message>
+    <message>
+        <source>Number of edges</source>
+        <translation>Número de bordas</translation>
+    </message>
+    <message>
+        <source>Ellipse by 4 points</source>
+        <translation>Elipse por 4 pontos</translation>
+    </message>
+    <message>
+        <source>Settings</source>
+        <translation>Configuração</translation>
+    </message>
+    <message>
+        <source>&amp;CADDigitize</source>
+        <translation>&amp;CADDigitize</translation>
+    </message>
+    <message>
+        <source>Ellipse from center</source>
+        <translation>Elipse por centro</translation>
+    </message>
+    <message>
+        <source>Circle by 2 tangents</source>
+        <translation>Círculo por 2 tangentes</translation>
+    </message>
+    <message>
+        <source>Number of points</source>
+        <translation>Numero de pontos</translation>
+    </message>
+</context>
+</TS>
